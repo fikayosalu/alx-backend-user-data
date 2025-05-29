dccf20f058356215e423aa4376b352c7c6d710d2
fikayosalu <salufikayo@gmail.com>
2025-05-29 12:10:59 +0100

    Added Documentation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.basic_auth import BasicAuth
+from typing import NoReturn
 
 
 app = Flask(__name__)
@@ -24,15 +25,17 @@ else:
 
 path = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
+
 @app.before_request
-def handle_before_request():
-    """TTH"""
-    if auth is None or auth.require_auth(request.path, path) == False:
+def handle_before_request() -> NoReturn:
+    """ Call before any request """
+    if auth is None or auth.require_auth(request.path, path) is False:
         return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
